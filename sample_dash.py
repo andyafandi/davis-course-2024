b2b71c9c4a0c714197ea9e4b7ea956430d5a0a9c
andyafandi <106495540+andyafandi@users.noreply.github.com>
2024-05-05 12:15:55 +0700

    Update sample_dash.py

--- sample_dash.py
+++ sample_dash.py
@@ -1,6 +1,6 @@
 import plotly.graph_objs as go
 import plotly.express as px
-import dash
+# import dash
 from dash import dcc
 # from dash import dhc as html
 # import dash_core_components as dcc
@@ -12,29 +12,30 @@ y_data = [10, 15, 13, 17, 20]
 
 # Line chart
 line_chart = go.Figure(data=go.Scatter(x=x_data, y=y_data, mode='lines', name='Line Chart'))
-
+line_chart.show()
 # Bar chart
 bar_chart = go.Figure(data=go.Bar(x=x_data, y=y_data))
-
+bar_chart.show()
 # Scatter plot
 scatter_plot = px.scatter(x=x_data, y=y_data, title='Scatter Plot')
-
+scatter_plot.show()
 # Heatmap
 heatmap_data = [[10, 20, 30], [40, 50, 60], [70, 80, 90]]
 heatmap = go.Figure(data=go.Heatmap(z=heatmap_data, colorscale='Viridis'))
+heatmap.show()
 
 # Create Dash application
-app = dash.Dash(__name__)
+# app = dash.Dash(__name__)
 
-# Define layout
-app.layout = html.Div(children=[
-    html.H1("Interactive Data Visualization with Plotly and Dash"),
-    dcc.Graph(id='line-chart', figure=line_chart),
-    dcc.Graph(id='bar-chart', figure=bar_chart),
-    dcc.Graph(id='scatter-plot', figure=scatter_plot),
-    dcc.Graph(id='heatmap', figure=heatmap)
-])
+# # Define layout
+# app.layout = html.Div(children=[
+#     html.H1("Interactive Data Visualization with Plotly and Dash"),
+#     dcc.Graph(id='line-chart', figure=line_chart),
+#     dcc.Graph(id='bar-chart', figure=bar_chart),
+#     dcc.Graph(id='scatter-plot', figure=scatter_plot),
+#     dcc.Graph(id='heatmap', figure=heatmap)
+# ])
 
-# Run the app
-if __name__ == '__main__':
-    app.run_server(debug=True)
+# # Run the app
+# if __name__ == '__main__':
+#     app.run_server(debug=True)
