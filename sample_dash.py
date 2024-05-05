2ca5fbf2ddc2ff47d9e7c72f23ce485943d06472
andyafandi <106495540+andyafandi@users.noreply.github.com>
2024-05-05 12:43:13 +0700

    Update sample_dash.py

--- sample_dash.py
+++ sample_dash.py
@@ -1,5 +1,6 @@
 import plotly.graph_objs as go
 import plotly.express as px
+import streamlit as st
 # import dash
 # from dash import dcc
 # # from dash import dhc as html
@@ -12,17 +13,24 @@ y_data = [10, 15, 13, 17, 20]
 
 # Line chart
 line_chart = go.Figure(data=go.Scatter(x=x_data, y=y_data, mode='lines', name='Line Chart'))
-line_chart.show()
+# line_chart.show()
+st.plotly_chart(line_chart, use_container_width=True)
+
 # Bar chart
 bar_chart = go.Figure(data=go.Bar(x=x_data, y=y_data))
-bar_chart.show()
+# bar_chart.show()
+st.plotly_chart(bar_chart, use_container_width=True)
+
 # Scatter plot
 scatter_plot = px.scatter(x=x_data, y=y_data, title='Scatter Plot')
-scatter_plot.show()
+# scatter_plot.show()
+st.plotly_chart(scatter_plot, use_container_width=True)
+
 # Heatmap
 heatmap_data = [[10, 20, 30], [40, 50, 60], [70, 80, 90]]
 heatmap = go.Figure(data=go.Heatmap(z=heatmap_data, colorscale='Viridis'))
-heatmap.show()
+# heatmap.show()
+st.plotly_chart(heatmap, use_container_width=True)
 
 # Create Dash application
 # app = dash.Dash(__name__)
