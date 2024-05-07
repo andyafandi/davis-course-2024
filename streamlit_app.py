3cf9e95a715ec4128efdcc6ddcaf6cc237305f89
andyafandi <106495540+andyafandi@users.noreply.github.com>
2024-05-07 07:56:25 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import plotly.graph_objs as go
 import plotly.express as px
 import streamlit as st
+import pandas as pd
 # import dash
 # from dash import dcc
 # # from dash import dhc as html
@@ -67,25 +68,26 @@ df = pd.DataFrame(data_sample)
 
 st.write('Hello, *World!* :sunglasses:')
 # Line chart
-line_chart = go.Figure(data=go.Scatter(x=x_data, y=y_data, mode='lines', name='Line Chart'))
+# line_chart = go.Figure(data=go.Scatter(x=x_data, y=y_data, mode='lines', name='Line Chart'))
+line_chart = go.Figure(data=go.Scatter(x=data_sample['company'], y=data_sample['sales'], mode='lines', name='Line Chart'))
 # line_chart.show()
 st.plotly_chart(line_chart, use_container_width=True)
 
 # Bar chart
-bar_chart = go.Figure(data=go.Bar(x=x_data, y=y_data))
-# bar_chart.show()
-st.plotly_chart(bar_chart, use_container_width=True)
+# bar_chart = go.Figure(data=go.Bar(x=x_data, y=y_data))
+# # bar_chart.show()
+# st.plotly_chart(bar_chart, use_container_width=True)
 
-# Scatter plot
-scatter_plot = px.scatter(x=x_data, y=y_data, title='Scatter Plot')
-# scatter_plot.show()
-st.plotly_chart(scatter_plot, use_container_width=True)
+# # Scatter plot
+# scatter_plot = px.scatter(x=x_data, y=y_data, title='Scatter Plot')
+# # scatter_plot.show()
+# st.plotly_chart(scatter_plot, use_container_width=True)
 
-# Heatmap
-heatmap_data = [[10, 20, 30], [40, 50, 60], [70, 80, 90]]
-heatmap = go.Figure(data=go.Heatmap(z=heatmap_data, colorscale='Viridis'))
-# heatmap.show()
-st.plotly_chart(heatmap, use_container_width=True)
+# # Heatmap
+# heatmap_data = [[10, 20, 30], [40, 50, 60], [70, 80, 90]]
+# heatmap = go.Figure(data=go.Heatmap(z=heatmap_data, colorscale='Viridis'))
+# # heatmap.show()
+# st.plotly_chart(heatmap, use_container_width=True)
 
 # Create Dash application
 # app = dash.Dash(__name__)
