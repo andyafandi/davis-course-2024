edef9a93bfda9e78301565919bde6e075c7aeb8b
andyafandi <106495540+andyafandi@users.noreply.github.com>
2024-05-10 08:56:18 +0700

    Update app2.py

--- app2.py
+++ app2.py
@@ -15,8 +15,8 @@ st.bar_chart(chart_data)
 conn = st.connection("mydb", type="sql", autocommit=True)
 
 # Perform query.
-df = conn.query('SELECT * from dimpromotion limit 10;', ttl=600)
+df = conn.query('SELECT EnglishPromotionName, StartDate, EndDate, MaxQty from dimpromotion limit 10;', ttl=600)
 
 # Print results.
 for row in df.itertuples():
-    st.write(f"{row.name} has a :{row.pet}:")
+    st.write(f"{row.EnglishPromotionName} , {row.MaxQty} ")
