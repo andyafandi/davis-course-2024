1abbdd3538215a039b14672da2904ad3f7293b2d
andyafandi <106495540+andyafandi@users.noreply.github.com>
2024-05-10 08:14:30 +0700

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,7 +1,20 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import os
 
 chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
 
 st.bar_chart(chart_data)
+
+
+
+# Initialize connection.
+conn = st.connection('mysql', type='sql', username=st.secrets["DB_USER"], password=st.secrets["DB_PASS"], host=st.secrets["HOST"], database=st.secrets["DB"])
+
+# Perform query.
+df = conn.query('SELECT * from dimpromotion limit 10;', ttl=600)
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.pet}:")
