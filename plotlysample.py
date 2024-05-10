9e8fa1d01ed53d6486a2c7b41918bbcc17ab4648
andyafandi <106495540+andyafandi@users.noreply.github.com>
2024-05-10 10:25:38 +0700

    Update plotlysample.py

--- plotlysample.py
+++ plotlysample.py
@@ -1,20 +1,30 @@
+# import streamlit as st
+# import numpy as np
+# # import plotly.figure_factory as ff
+# from plotly.figure_factory import create_distplot
+
+# # Add histogram data
+# x1 = np.random.randn(200) - 2
+# x2 = np.random.randn(200)
+# x3 = np.random.randn(200) + 2
+
+# # Group data together
+# hist_data = [x1, x2, x3]
+
+# group_labels = ['Group 1', 'Group 2', 'Group 3']
+
+# # Create distplot with custom bin_size
+# fig = create_distplot(hist_data, group_labels, bin_size=[.1, .25, .5])
+
+# # Plot!
+# st.plotly_chart(fig, use_container_width=True)
+
 import streamlit as st
+import matplotlib.pyplot as plt
 import numpy as np
-# import plotly.figure_factory as ff
-from plotly.figure_factory import create_distplot
 
-# Add histogram data
-x1 = np.random.randn(200) - 2
-x2 = np.random.randn(200)
-x3 = np.random.randn(200) + 2
+arr = np.random.normal(1, 1, size=100)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
 
-# Group data together
-hist_data = [x1, x2, x3]
-
-group_labels = ['Group 1', 'Group 2', 'Group 3']
-
-# Create distplot with custom bin_size
-fig = create_distplot(hist_data, group_labels, bin_size=[.1, .25, .5])
-
-# Plot!
-st.plotly_chart(fig, use_container_width=True)
+st.pyplot(fig)
